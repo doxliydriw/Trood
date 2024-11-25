bb06cd5fba1e7815588b8e7c009776a390d9926f
doxliydriw <123301390+doxliydriw@users.noreply.github.com>
2024-11-25 12:41:56 +0100

    fix

--- trood/vite.config.js
+++ trood/vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+
 export default defineConfig(({ command }) => ({
   base: command === 'build' ? '/Trood/' : '/',
   plugins: [react()],
